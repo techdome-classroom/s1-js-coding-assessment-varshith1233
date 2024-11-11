0dc12e59164f8cb2aa5ecded5c9a0847d910b2ef
Charan P <charanp@Charans-MacBook-Pro.local>
2024-11-11 13:46:33 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -2,10 +2,8 @@ const initializeDPTable = (messageLength, pattern) => {
   const patternLength = pattern.length;
   const dp = Array.from({ length: messageLength + 1 }, () => Array(patternLength + 1).fill(false));
 
-  // Base case: empty pattern matches empty message
   dp[0][0] = true;
 
-  // Initialize dp for patterns that start with '*'
   for (let j = 1; j <= patternLength; j++) {
     if (pattern[j - 1] === '*') {
       dp[0][j] = dp[0][j - 1];
