eb7725e647da11dd33f4c05e6ce6ee8cb48cbe41
Charan P <charanp@Charans-MacBook-Pro.local>
2024-11-11 13:48:43 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,34 +1,7 @@
 
 const getTotalIsles = function (grid) {
 
-    if (!grid || grid.length === 0) return 0;
-  
-    let islandsCount = 0;
-  
-    const dfs = (i, j) => {
-
-      if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] === 'W') return;
-  
-      grid[i][j] = 'W';
-  
-      
-      dfs(i + 1, j); 
-      dfs(i - 1, j); 
-      dfs(i, j + 1); 
-      dfs(i, j - 1); 
-    };
-  
-
-    for (let i = 0; i < grid.length; i++) {
-      for (let j = 0; j < grid[0].length; j++) {
-        if (grid[i][j] === 'L') { 
-          islandsCount++; 
-          dfs(i, j); 
-        }
-      }
-    }
-  
-    return islandsCount;
+   
   };
   
 module.exports = getTotalIsles;
