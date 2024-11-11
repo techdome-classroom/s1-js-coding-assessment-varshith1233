70d7fae60c21dc97b99bb21446a857a05d2253dc
Charan P <charanp@Charans-MacBook-Pro.local>
2024-11-11 13:48:38 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -1,33 +1,34 @@
-const countIslands = function(map) {
-  if (!map || map.length === 0) return 0;
 
-  let islandCount = 0;
+const getTotalIsles = function (grid) {
 
-  const exploreIsland = (x, y) => {
-    // Base case for recursion: if out of bounds or it's water, stop.
-    if (x < 0 || y < 0 || x >= map.length || y >= map[0].length || map[x][y] === 'W') return;
+    if (!grid || grid.length === 0) return 0;
+  
+    let islandsCount = 0;
+  
+    const dfs = (i, j) => {
 
-    // Mark the cell as visited by changing it to 'W'.
-    map[x][y] = 'W';
+      if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] === 'W') return;
+  
+      grid[i][j] = 'W';
+  
+      
+      dfs(i + 1, j); 
+      dfs(i - 1, j); 
+      dfs(i, j + 1); 
+      dfs(i, j - 1); 
+    };
+  
 
-    // Recursively visit adjacent cells in all four directions.
-    exploreIsland(x + 1, y); // down
-    exploreIsland(x - 1, y); // up
-    exploreIsland(x, y + 1); // right
-    exploreIsland(x, y - 1); // left
-  };
-
-  // Loop through each cell in the map grid.
-  for (let i = 0; i < map.length; i++) {
-    for (let j = 0; j < map[0].length; j++) {
-      if (map[i][j] === 'L') { // Unvisited land cell found
-        islandCount++; // Increment island count for each new island
-        exploreIsland(i, j); // Explore the entire island
+    for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[0].length; j++) {
+        if (grid[i][j] === 'L') { 
+          islandsCount++; 
+          dfs(i, j); 
+        }
       }
     }
-  }
-
-  return islandCount;
-};
-
-module.exports = countIslands;
+  
+    return islandsCount;
+  };
+  
+module.exports = getTotalIsles;
